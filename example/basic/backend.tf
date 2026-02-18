7b9b8ca08d8d4b49d3c7638d05a7ca5f17a3623f
Althaf Ahmed Md <althaf.ahmed@atyeti.com>
2026-02-18 11:26:05 +0530

    Update backend.tf

--- example/basic/backend.tf
+++ example/basic/backend.tf
@@ -1,9 +1,14 @@
-terraform {
+/*terraform {
   backend "remote" {
     organization = "atyeti-inc"
 
     workspaces {
       prefix = "WAForge-landing-zone"
     }
+  }
+}*/
+terraform {
+  backend "local" {
+    path = "terraform.tfstate"
   }
 }
